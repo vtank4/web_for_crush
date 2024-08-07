fba40439a954cf6da3908eec816dbdafb93b6be2
Ba Viet Anh Nguyen <129357925+vtank4@users.noreply.github.com>
2024-08-07 20:33:14 +0700

    Add files via upload

---
+++ confess-crush-main/confess-crush-main/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Vank và Tank",
+    introTitle: 'Le Thi Tank',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu Vank ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu Vank <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Hôm nay là kỉ niệm 1 năm 3 tháng a với e quen nhau rùi nè. A iu e nhìu lắmmmmm. \n Đừng có khóc nhè, đừng có bùn nữa bikkkk chuawaaa đồ cuteeee',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: '' 
+}
